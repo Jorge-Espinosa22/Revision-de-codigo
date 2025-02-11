34a1e9179ecf868751c4550f2762889d31f2221d
Jorge Espinosa <jorgeusharif@gmail.com>
2025-02-10 21:07:45 -0600

    Codigo resuelto y mejorado.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
+
+
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const nameUser = document.querySelector('.name');//Cambio de nombre a variables a nombres mas intuitivos.
+const blog = document.querySelector('.blog');//Cambio de # a . para entrar a los atributos
+const userLocation = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+const displayUser = async (username) => {//Como tiene una promesa le agregue Async
+  try { //Se agrego try para utilizar las promesas
+    nameUser.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    nameUser.textContent = `${data.name}`;
+    blog.textContent = `${data.blog}`;
+    userLocation.textContent = `${data.location}`;
+  } 
+  catch (error) { //Se agrego catch y se elimino la funcion para reducir lineas de codigo
+    console.log('OH NO!');
+    console.log(error);
+    nameUser.textContent = `Algo salió mal: ${error}`
+  }
+
 }
+  displayUser('stolinski');
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
-
-displayUser('stolinski').catch(handleError);
